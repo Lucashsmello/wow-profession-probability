ec60882b62c9530407e8719255278a8dd396ecbc
Lucashsmello <lucashsmello@gmail.com>
2021-06-05 07:28:02 -0300

    TBC data

--- download_profession.py
+++ download_profession.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import argparse
+import string
 
 parser = argparse.ArgumentParser(description='Downloads difficulty skill level for each recipe/item')
 parser.add_argument('-o', '--outfile', type=str, help='File out', required=True)
@@ -25,8 +26,7 @@ def item2Str(item):
 
 
 professions = ['alchemy', 'blacksmithing', 'cooking', 'tailoring', 'engineering',
-               'leatherworking', 'enchanting', 'first_aid', 'mining',
-               ]
+               'leatherworking', 'enchanting', 'first-aid', 'mining', 'jewelcrafting']
 # url_template='https://classic.wowhead.com/%s'
 url_template = 'https://tbc.wowhead.com/%s'
 
@@ -35,6 +35,13 @@ with open(args.outfile, 'w') as fout:
         print(prof)
         data = getData(url_template % prof.replace('_', '-'))
         data_str = [item2Str(item) for item in data if('colors' in item)]
-        fout.write('\nlocal %s = {\n' % prof)
+        fout.write('\nlocal %s = {\n' % prof.replace('-', '_'))
         fout.write(",\n".join(data_str))
         fout.write('\n};\n')
+
+    fout.write("\n")
+    fout.write("local professions_table = {\n")
+    profcode = ['\t["%s"] = %s' % (string.capwords(prof.replace('-', ' ')), prof.replace('-', '_'))
+                for prof in professions]
+    fout.write(",\n".join(profcode))
+    fout.write("\n};")
